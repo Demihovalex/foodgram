6a9aefd65565d6059b2c2dc7bf6db3becb1a23ff
Alexander Demihov <demihov.alex@yandex.ru>
2026-05-30 00:31:47 +0300

    Fix admin validation: show error for missing ingredients

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django import forms
+from django.core.exceptions import ValidationError
+
 from users.models import Subscription
 
 from .models import (
@@ -11,10 +14,47 @@ from .models import (
 )
 
 
+class RecipeIngredientInlineForm(forms.ModelForm):
+    class Meta:
+        model = RecipeIngredient
+        fields = '__all__'
+
+
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
+    form = RecipeIngredientInlineForm
     min_num = 1
     extra = 1
+
+    def get_formset(self, request, obj=None, **kwargs):
+        formset = super().get_formset(request, obj, **kwargs)
+        
+        def clean_formset(formset):
+            """Проверка, что есть хотя бы один ингредиент"""
+            has_ingredients = False
+            for form in formset.forms:
+                if form.cleaned_data and not form.cleaned_data.get(
+                    'DELETE', False):
+                    has_ingredients = True
+                    break
+            if not has_ingredients:
+                raise ValidationError(
+                    'Рецепт должен содержать хотя бы один ингредиент')
+        formset.clean = clean_formset
+        return formset
+
+
+class RecipeAdminForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super().clean()
+        image = cleaned_data.get('image')
+        if not image:
+            self.add_error('image', 'Рецепт должен содержать изображение')
+        return cleaned_data
 
 
 @admin.register(Tag)
@@ -33,26 +73,16 @@ class IngredientAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
+    form = RecipeAdminForm
+    inlines = [RecipeIngredientInline]
     list_display = ("id", "name", "author", "cooking_time", "pub_date")
     search_fields = ("name", "author__email", "author__username")
     list_filter = ("tags",)
-    inlines = (RecipeIngredientInline,)
     readonly_fields = ("favorite_count",)
 
     def favorite_count(self, obj):
         return obj.favorites.count()
     favorite_count.short_description = "Добавлений в избранное"
-
-    def save_model(self, request, obj, form, change):
-        if not obj.image:
-            raise ValueError("Рецепт должен содержать изображение")
-        super().save_model(request, obj, form, change)
-
-    def save_related(self, request, form, formsets, change):
-        super().save_related(request, form, formsets, change)
-        obj = form.instance
-        if not obj.ingredients.exists():
-            raise ValueError("Рецепт должен содержать хотя бы один ингредиент")
 
 
 @admin.register(Subscription)
